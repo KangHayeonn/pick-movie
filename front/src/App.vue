ab107cf08b08834b363a4b33dd5149dff6bb304a
Lydia <78534650+KangHayeonn@users.noreply.github.com>
2022-06-26 10:23:13 +0900

    Update App.vue

--- front/src/App.vue
+++ front/src/App.vue
@@ -2,7 +2,7 @@
 	<div id="app">
 		<img alt="Vue logo" src="./assets/logo.png" />
 		<HelloWorld msg="Pick Movie App" />
-		<div>Front CI 테스트2</div>
+		<div>Front CI 테스트2 - Directory url 변경</div>
 	</div>
 </template>
 

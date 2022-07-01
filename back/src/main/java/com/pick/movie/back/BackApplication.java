8ca4c851df43f9a995170585303fead94651e7a8
SoNgSeUNgMiN96 <tmd9544@gmail.com>
2022-07-01 20:45:14 +0900

    [test] : add deploy authentication

--- back/src/main/java/com/pick/movie/back/BackApplication.java
+++ back/src/main/java/com/pick/movie/back/BackApplication.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
-
+//test
 @SpringBootApplication
 public class BackApplication {
 

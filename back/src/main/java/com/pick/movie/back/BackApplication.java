f8fd68ebad92764c0d56549ee5f5455e6dfb9819
SoNgSeUNgMiN96 <tmd9544@gmail.com>
2022-07-01 21:03:52 +0900

    [test] : codeDeploy test

--- back/src/main/java/com/pick/movie/back/BackApplication.java
+++ back/src/main/java/com/pick/movie/back/BackApplication.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
-//test
 @SpringBootApplication
 public class BackApplication {
 

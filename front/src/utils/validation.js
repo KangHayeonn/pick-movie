280b13ff40a1d7f96f10292f504db9b16f4baf58
yeoni sugar <rkdgkdus522@naver.com>
2022-10-24 01:30:01 +0900

    [add]: 회원가입 validation 기능 및 예외처리 구현

--- front/src/utils/validation.js
+++ front/src/utils/validation.js
@@ -1,14 +1,13 @@
 // ReDoS 에 취약한 문제 때문에 삭제
 export const _isValidEmail = email => {
   const regex =
-    /[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]$/i
-  return email != '' && email != 'undefined' && regex.test(email)
+    /([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
+  return email != 'undefined' && regex.test(email)
 }
 
 export const _isValidPassword = password => {
   const regex = /^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/
   return (
-    password != '' &&
     password != 'undefined' &&
     regex.test(password) &&
     password.search(/\s/) == -1 // 공백

94e84453b50e2865767dcd791091473ddbd0ac6a
SoNgSeUNgMiN96 <87978544+SoNgSeUNgMiN96@users.noreply.github.com>
2022-10-24 00:51:25 +0900

    [fix] : swagger type fixed

--- back/src/main/java/com/pick/movie/back/dto/SignupDto.java
+++ back/src/main/java/com/pick/movie/back/dto/SignupDto.java
@@ -1,12 +1,16 @@
 package com.pick.movie.back.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 public class SignupDto {
+
+    @ApiModelProperty(example = "test1212@naver.com")
     private String username;
+    @ApiModelProperty(example = "test1212!")
     private String password;
     private List<String> tags;
 }
